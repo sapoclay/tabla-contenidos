4a7942d2c21871b975be6d84ce5e7e5f026f629d
SapoClay <siyofueramuyrico@gmail.com>
2019-11-12 03:35:45 +0100

    Update tabla-contenidos.js

--- tabla-contenidos.js
+++ tabla-contenidos.js
@@ -1,17 +1,18 @@
 (function($){
 
 	if ( $('body.single').length ){
-
-		//Plantillas para la tabla
-		var tmplwrap ="<div id='tabla-contenido'>\n<p class='titulo'>Tabla de Contenido</p>\n{contenido}</div>";
-		var tmpllink = "<p {clase}><i class='fa fa-caret-right'></i> <a href={link}>{texto}</a></p>\n";
+		//Enlaces para mostrar u ocultar la tabla de contenidos
+		var botonMenu = "<p><div id='botonMO'>CONTENIDO - [<a href='javascript:mostrar();'>Mostrar</a> / <a href='javascript:cerrar();''>Ocultar</a>]</div></p>";
+		//Plantillas para la tabla. Por defecto está oculta
+		var tmplwrap = "<div id='tabla-contenido' style='display:none;'>\n<p class='titulo'>Tabla de Contenido </p>\n{contenido}</div>";
+		var tmpllink = "<p {clase}><i class='fa fa-caret-right'></i> <a href={link} class='enlaces'>{texto}</a></p>\n";
 		var cadena 	= "";
 		
-		//Preparamos el segundo y tercer nivel como clases
+		//Preparamos el segundo y tercer nivel para que tenga una clase
 		$('article h2').siblings('h3').addClass('n2');
 		$('article h2').siblings('h4').addClass('n3');
 
-		//El bucle. Recorremos elementos para construir la tabla
+		//Bucle, recorremos elementos para construir la tabla
 		$('article h2, article h3, article h4').each(function(index, element){
 	
 			clase = "";
@@ -32,16 +33,34 @@
 
 		});
 
-		//Ejemplo de link adicional. Aquí habrá que cambiar el enlace por el ID de los artículos relacionados de cada web
+		//Ejemplo de link adicional, descomentar
+
+		$('div.dc-social h3').attr('id','titulo-dc-social');
+		cadena +="<p><i class='fa fa-share-alt'></i> <a href='#titulo-dc-social'>Compartir</a></p>\n";
+
+
+		//artículos relacionados
 
 		$('div.rel_posts h3').attr('id','quizasteinterese');
 		cadena +="<p><i class='fa fa-link'></i> <a href='#quizasteinterese'>Artículos relacionados</a></p>\n";
 
 		cadena = tmplwrap.replace('{contenido}',cadena);
 
-		//Insertamos la cadena antes del primer elemento h2
+		//Finalmente insertamos la cadena antes del primer elemento
+		$(botonMenu).insertBefore( $('.entry-content h2').first() );
+
 		$(cadena).insertBefore( $('.entry-content h2').first() );
 	}
 
-
 })(jQuery);
+
+//funciones para mostrar u ocultar la tabla de contenido
+function mostrar() {
+            div = document.getElementById('tabla-contenido');
+            div.style.display = '';
+        }
+
+function cerrar() {
+            div = document.getElementById('tabla-contenido');
+            div.style.display = 'none';
+        }
